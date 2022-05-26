aafe5f45b53a6972c969e5d82836fca13b1ad86d
linnefromice <linnefromice@gmail.com>
2022-05-26 10:34:11 +0900

    feat: update logic to use ipfs

--- src/mapping-pool.ts
+++ src/mapping-pool.ts
@@ -1,6 +1,8 @@
-import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt, ipfs, json } from "@graphprotocol/graph-ts";
 import { Deposited, Pool } from "../generated/Pool/Pool"
-import { Pool as PoolEntity } from "../generated/schema"
+import { Pool as PoolEntity, Price as PriceEntity } from "../generated/schema"
+
+const HASH = "QmUDic6ocMVUm1yTA3ogic8CUvehg4PuxXhuVuSBWQW8i6"
 
 function getPool(address: Address): PoolEntity {
   const entity = PoolEntity.load(address.toHexString())
@@ -17,9 +19,29 @@ function getPool(address: Address): PoolEntity {
   return newEntity
 }
 
+function getPrice(timestamp: BigInt): void {
+  let entity = PriceEntity.load("prices")
+  if (!entity) {
+    entity = new PriceEntity("prices")
+  }
+  const data = ipfs.cat(HASH)
+  if (data) {
+    const _json = json.fromBytes(data)
+    console.log(_json.toString())
+    const _price = _json.toObject().get("MUUU")
+    entity.priceInUsd = _price ? _price.toBigInt().toBigDecimal() : BigDecimal.zero()
+    entity.updatedAt = timestamp
+    entity.save()
+  } else {
+    console.log("Cannot get data from ipfs")
+  }
+}
+
 export function handleDeposit(event: Deposited): void {
   const entity = getPool(event.address)
   entity.totalSupply = entity.totalSupply.plus(event.params.amount.toBigDecimal())
   entity.depositCount = entity.depositCount.plus(BigInt.fromString("1"))
   entity.save()
+  // update price
+  getPrice(event.block.timestamp)
 }
